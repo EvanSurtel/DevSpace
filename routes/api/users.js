8e74be640e4851ed32cd0755fc8e7fd47ddfebb1
evansurtel <evan.j.surtel@gmail.com>
2022-11-11 11:56:33 -0500

    Request and body Validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,22 @@
 //registering users 
 const express = require('express');
 const router = express.Router();
+const {check, validationResult} = require('express-validator/check');
 
-// @route   GET api/users
-// @desc    TEST route
+// @route   POST api/users
+// @desc    Rgister user
 // @access  Public
 
-router.get('/', (req, res) => {
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email','Please include email').isEmail(),
+    check('password','Please enter a password with 6 or more characters').isLength({min: 6})
+],
+(req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
     console.log(req.body);//object of data that will be sent to this route; we have to initialize the middleware for the body parser
     res.send('User route')
 
