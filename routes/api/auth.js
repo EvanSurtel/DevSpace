30c5ed8b91f830f6cc77c15cd7166f3be004166b
evansurtel <evan.j.surtel@gmail.com>
2022-11-18 15:42:31 -0500

    User authentication and login route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,13 +1,16 @@
 //get json wbtoken for authentication
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const auth = require('../../middleware/auth')
-
+const {check, validationResult} = require('express-validator'); 
 const User = require('../../models/User');
-// @route   GET api/auth
-// @desc    TEST route
-// @access  Public
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
+// @route  Get api/auth
+// @desc    Test route
+// @access  Public
 router.get('/', auth, async (req, res) => {//protected route
     try{//do not want password 
         const user = await User.findById(req.user.id).select('-password');//since this is protected route and we used token that has the id and middleware we set req.user to user in the token, we can pass in req.user. we can access req.user anywhere in a protected route.
@@ -16,6 +19,53 @@ router.get('/', auth, async (req, res) => {//protected route
         console.error(err.message);
         res.status(500).send('Server Error');
     }
-});//test route
+});
+// @route  Post api/auth
+// @desc    Authenticate user and get token
+// @access  Public
+router.post('/', [
+    check('email', 'Valid email is required').isEmail(),
+    check('password', 'Password is required ').exists()
+],
+async (req,res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+    const { email, password } = req.body;
+    try {
+        let user = await User.findOne({email});//since this returns promise, if user exists then the credentials will mbe stores in user
+        if(!user) {
+            return res.status(400).json({msg : 'Invalid Credentials'})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);//since above let user = await User.findOne({email}); returns promise, we can access user.password which is encypted
+
+        if(!isMatch) {
+            return res.status(400).json({ msg: 'Invalid Credentials'});
+        }
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+        
+        jwt.sign(
+            payload,
+            process.env.jwtSecret,
+            { expiresIn: 360000},
+            (err, token) => {
+                if(err) throw err;
+                res.json({ token });
+            }
+
+        );
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
 
 module.exports = router;
