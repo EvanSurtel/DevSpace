b8bcfa9a7d7d8175f28be89516e1bdb635ddceb5
evansurtel <evan.j.surtel@gmail.com>
2022-11-26 17:34:13 -0500

    Get all profiles and get profile by user ID

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -101,4 +101,38 @@ async (req, res) =>{
     }
 });
 
+// @route   GET api/profile
+// @desc    Get all user profiles
+// @access  Public
+
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+        res.json(profiles);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500). send('Server Error');
+    }
+})
+
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile from user ID
+// @access  Public
+
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.params.user_id}).populate('user', ['name', 'avatar']);
+        if(!profile){
+            return res.status(400).json({ msg: 'Profile not found'});
+        }
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        console.error(err.message);
+        if(err.kind == 'ObjectId') return res.status(401).json({ msg: 'Profile not found'});
+        
+        res.status(500). send('Server Error');
+    }
+})
+
 module.exports = router;
