211a39d40d092f0fdca312709d000572d45fc55d
evansurtel <evan.j.surtel@gmail.com>
2022-12-09 22:54:09 -0500

    Frontend routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,27 @@
 import React, { Fragment } from 'react';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import { HashRouter as Router, Route, Routes, Switch } from 'react-router-dom';
 import './App.css';
+import { Provider } from 'react-redux';
+import store from './store';
 
-const App = () => (
-	<Fragment>
-		<Navbar />
-		<Landing />
-	</Fragment>
-);
+const App = () => {
+	return (
+		<Router>
+			<Navbar />
+			{/* <Landing /> */}
+
+			<Routes>
+				<Route path='/' element={<Landing />} />
+
+				<Route path='/register' element={<Register />} />
+				<Route path='/login' element={<Login />} />
+			</Routes>
+		</Router>
+	);
+};
 
 export default App;
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,25 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
 	return (
-		<nav class='navbar bg-dark'>
-			<h1>
-				<a href='index.html'>
-					<i class='fas fa-code'></i> DevSpace
-				</a>
-			</h1>
-			<ul>
-				<li>
-					<a href='profiles.html'>Developers</a>
-				</li>
-				<li>
-					<a href='register.html'>Register</a>
-				</li>
-				<li>
-					<a href='login.html'>Login</a>
-				</li>
-			</ul>
-		</nav>
+		<div>
+			<nav className='navbar bg-dark'>
+				<h1>
+					<Link to='/'>
+						<i className='fas fa-code'></i> DevConnector
+					</Link>
+				</h1>
+				<ul>
+					<li>
+						<Link to='/profiles'>Developers</Link>
+					</li>
+					<li>
+						<Link to='/register'>Register</Link>
+					</li>
+					<li>
+						<Link to='/login'>Login</Link>
+					</li>
+				</ul>
+			</nav>
+		</div>
 	);
 };
 
