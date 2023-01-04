481bd470e7f72fc26450440aa82d6f1654f72011
evansurtel <evan.j.surtel@gmail.com>
2023-01-03 20:20:39 -0500

    Loading/Authenticating user from front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,41 @@
-import React, { Fragment } from 'react';
+import React, { useEffect } from 'react';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+import { loadUser } from './actions/auth';
 import { HashRouter as Router, Route, Routes, Switch } from 'react-router-dom';
 import './App.css';
 //Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import setAuthToken from './utils/setAuthToken';
 
-const App = () => (
-	<Provider store={store}>
-		<Router>
-			<Navbar />
-			{/* <Landing /> */}
-			<Alert />
-			<Routes>
-				<Route path='/' element={<Landing />} />
+const App = () => {
+	useEffect(() => {
+		if (localStorage.token) {
+			setAuthToken(localStorage.token);
+		}
+		store.dispatch(loadUser());
+	}, []);
 
-				<Route path='/register' element={<Register />} />
-				<Route path='/login' element={<Login />} />
-			</Routes>
-		</Router>
-	</Provider>
-);
+	return (
+		<Provider store={store}>
+			<Router>
+				<Navbar />
+				{/* <Landing /> */}
+				<Alert />
+				<Routes>
+					<Route path='/' element={<Landing />} />
+
+					<Route path='/register' element={<Register />} />
+					<Route path='/login' element={<Login />} />
+				</Routes>
+			</Router>
+		</Provider>
+	);
+};
 //change
 
 export default App;
