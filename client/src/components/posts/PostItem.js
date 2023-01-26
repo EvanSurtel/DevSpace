12ce0ea72a47408c56a31d4c1477c917729bec78
evansurtel <evan.j.surtel@gmail.com>
2023-01-26 12:18:43 -0500

    Unlike post front end

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -2,14 +2,14 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { addLike } from '../../actions/post';
+import { addLike, removeLike } from '../../actions/post';
 import formatDate from '../../utils/formatDate';
 
 const PostItem = ({
 	post: { _id, user, text, name, avatar, likes, comments, date },
 	auth,
-	showActions,
 	addLike,
+	removeLike,
 }) => (
 	<div className='post bg-white p-1 my-1'>
 		<div>
@@ -22,18 +22,19 @@ const PostItem = ({
 			<p className='my-1'>{text}</p>
 			<p className='post-date'>Posted on {formatDate(date)}</p>
 
-			{showActions && (
-				<Fragment>
-					<button
-						onClick={() => addLike(_id)}
-						type='button'
-						className='btn btn-light'
-					>
-						<i className='fas fa-thumbs-up' />{' '}
-						<span>{likes.length > 0 && <span>{likes.length}</span>}</span>
-					</button>
-				</Fragment>
-			)}
+			<Fragment>
+				<button
+					onClick={() => addLike(_id)}
+					type='button'
+					className='btn btn-light'
+				>
+					<i className='fas fa-thumbs-up' />{' '}
+					<span>{likes.length > 0 && <span>{likes.length}</span>}</span>
+				</button>
+				<button className='btn btn-light' onClick={() => removeLike(_id)}>
+					<i className='fas fa-thumbs-down' />
+				</button>
+			</Fragment>
 		</div>
 	</div>
 );
@@ -42,14 +43,11 @@ PostItem.propTypes = {
 	post: PropTypes.object.isRequired,
 	auth: PropTypes.object.isRequired,
 	addLike: PropTypes.func.isRequired,
+	removeLike: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
 	auth: state.auth,
 });
 
-PostItem.defaultProps = {
-	showActions: true,
-};
-
-export default connect(mapStateToProps, { addLike })(PostItem);
+export default connect(mapStateToProps, { addLike, removeLike })(PostItem);
