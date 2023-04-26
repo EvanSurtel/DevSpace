69a276ff8ba0fca7aa2c78c749e23ac985d329a8
evansurtel <evan.j.surtel@gmail.com>
2023-04-26 16:32:03 -0400

    heroku deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express'); //get express server up and running
 const connectDB = require('./config/db');
 require('dotenv').config();
+const path = require('path');
 
 const app = express(); // initialize app using express
 
@@ -9,13 +10,21 @@ connectDB(); //connect to database
 //Initialize middleware
 app.use(express.json({ extended: false })); //allowing to get data in req.body
 
-app.get('/', (req, res) => res.send('API running')); //create single endpoint to test; callback with request response, re.send will send data to browser
-
 //define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const PORT = process.env.PORT || 5001; //initialize port; look for environment variable called PORT; when we employ to heroku that is where it will ge PORT number; if there is no env variable set will automatically go to port 5001
 
